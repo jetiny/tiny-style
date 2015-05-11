20dda73ac7a6296a7c55187fa48d03d6e204600e
jetiny <jetiny8@gmail.com>
2015-05-11 17:42:49 +0800

    add asset class

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var parseSource = require("./lib/parseSource"),
-	ReplaceMany = require("./lib/ReplaceMany");
+	ReplaceMany = require("./lib/ReplaceMany"),
+	queue = require('tiny-queue');
 
 /*
 
@@ -20,20 +21,35 @@ var col = require('tiny-col');
 var url_part = '__IMPORT_URL__',
 	RE_URL_PART = new RegExp(url_part + '([0-9]+)', 'g');
 
-function Parser (content) {
-	var stuff = parseSource(content),
-		replacer = new ReplaceMany();
-	stuff.forEach(function (it, id) {
-		replacer.replace(it.start, it.length, url_part + id);
-	});
-	var cssContent = replacer.run(content);
-	var css = JSON.stringify(cssContent);
-	css = css.replace(RE_URL_PART, function (str, id) {
-		console.log(stuff[id]);
-	});
+function Parser (text, done, context) {
+	var stuff = parseSource(text),
+		replacer = new ReplaceMany(),
+		q = queue(context, done || function (err, css) {
+			if (err)
+				col.error(err);
+			else
+				col.done(css);
+		}, stuff.map(function (it) {
+			return function (next) {
+				next();
+			};
+		}));
+	q.next(function (next) {
+		stuff.forEach(function (it, id) {
+			//col('isRelativeUrl', it.isRelativeUrl(), 'isDataUrl', it.isDataUrl(), 'isAbsoulteUrl', it.isAbsoulteUrl() ,it.url);
+			replacer.replace(it.start, it.length, url_part + id);
+		});
+		var cssContent = replacer.run(text);
+		var css = JSON.stringify(cssContent);
+		css = css.replace(RE_URL_PART, function (str, id) {
+			return stuff[id].data;
+		});
+		next(null, css);
+	})
+	.start();
 }
 
-//Parser("@import url( 'test.css' );\n.class { a: b c d; }");
-//Parser("@import url('~test/css') screen and print;\n.class { a: b c d; }");
-//Parser(".class { background: green url( \"img.png\" ) xyz }");
+Parser("@import url( 'test.css' );\n.class { a: b c d; }");
+Parser("@import url('~test/css') screen and print;\n.class { a: b c d; }");
+Parser(".class { background: green url( \"img.png\" ) xyz }");
 Parser(".class { background: green url(data:image/png;base64,AAA) url(http://example.com/image.jpg) url(//example.com/image.png) xyz }");
