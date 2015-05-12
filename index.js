e8779fdcb00b259d7586131bdc2e0f379e741e8b
jetiny <jetiny8@gmail.com>
2015-05-12 17:40:41 +0800

    tmp functions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var parseSource = require("./lib/parseSource"),
 	ReplaceMany = require("./lib/ReplaceMany"),
-	queue = require('tiny-queue');
+	queue = require('tiny-queue'),
+	Url = require('./lib/url');
 
 /*
 
@@ -18,13 +19,12 @@ css 压缩
 var col = require('tiny-col');
 
 
-
-
-function createParser (opts) {
-	if (!opts) 
-		opts = {};
-	var url_part = opts.url_part || '__IMPORT_URL__',
+function createParser (options) {
+	if (!options) 
+		options = {};
+	var url_part = options.url_part || '__IMPORT_URL__',
 		RE_URL_PART = new RegExp(url_part + '([0-9]+)', 'g');
+	initOptions(options);
 	return function (text, filter, done, context) {
 		if (!filter)
 			filter = function (next) {
@@ -42,7 +42,7 @@ function createParser (opts) {
 			replacer = new ReplaceMany(),
 			q = queue(context, done, stuff.map(function (it) {
 				return function (next) {
-					filter(next, it);
+					filter(next, it, options);
 				};
 			}));
 		q.next(function (next) {
@@ -66,7 +66,27 @@ function createParser (opts) {
 //Parser(".class { background: green url( \"img.png\" ) xyz }");
 //Parser(".class { background: green url(data:image/png;base64,AAA) url(http://example.com/image.jpg) url(//example.com/image.png) xyz }");
 
-var Context = {};
+var Context = {
+	directory : './public',
+	assetsPaths: {
+		'images': ['png', 'jpg', 'gif', 'svg'] ,
+		'styles': ['css'] ,
+		'fonts':  ['woff', 'woff2', 'ttf', 'eot'] // 'svg'
+	},
+	combine : true,   // 合并外链css到一个文件
+	imageData: true,  // 将图片转换为data:image
+	imageDataFilters: [
+		function (img, asset) {
+			return false; // return true;
+		}
+	], //过滤器
+	imageFile: true,  // 将data:image 转换为文件
+	imageFileFilters: [
+		function (img, asset) {
+			return false; // return true;
+		}
+	], //过滤器
+};
 
 Context.load = function (url, cb) {
 	
@@ -76,25 +96,114 @@ Context.save = function (url, data, cb) {
 	
 };
 
+
 var Options = {
-	
 	stylePath:'./css',
 	imagePath:'./images',
 	fontPath:'./fonts',
-
-	extractImageDataUrl: true
+	extractImageDataUrl: true,
 };
+
+function initOptionsFilter(options, name, vals) {
+	if (!options[name]) {
+		options[name] = [];
+	};
+	var filters = options[name];
+	filters.push.apply(filters, vals);
+}
+
+function makeFileTypeFilter(type, extensions) {
+	var RE_FILETYPE = new RegExp('(' + extensions + ')$', 'ig');
+	return function(it){
+		if (RE_FILETYPE.test(it.fileName)) {
+			it.fileType = type;
+			return true;
+		}
+	}
+}
+
+function makeAssetPathFilter(type, dir) {
+	return function(it) {
+		if (it.fileType === 'type') {
+			it.distDir = dir;
+			return true;
+		}
+	}
+}
+
+function initOptions(options) {
+	initOptionsFilter(options, 'assets', [
+		makeAssetPathFilter('image', 'images'),
+		makeAssetPathFilter('font', 'fonts'),
+		makeAssetPathFilter('css', 'css')
+	]);
+	initOptionsFilter(options, 'fileType', [
+		makeFileTypeFilter('image', 'png|gif|jpg|svg|jpeg'),
+		makeFileTypeFilter('font', 'woff|woff2|ttf|eot'),
+		makeFileTypeFilter('css', 'css')
+	]);
+	options.emitFilters = function (type, context, func) {
+		var filters = getFilters(type, options, context);
+		if (filters.length) {
+			var r;
+			for(var i=0, l= filters.length-1; i<l; i++) {
+				r = func(filters[i]);
+				if (undefined !== r)
+					break;
+			}
+			return r;
+		}
+	}
+}
+
+function getFilters(type, opts, context) {
+	var dist = [],
+		opt_filters = opts && opts[type],
+		context_filters = context && context[type];
+	if (context_filters){
+		dist.push.apply(dist, context_filters.slice());
+	}
+	if (opt_filters){
+		dist.push.apply(dist, opt_filters.slice());
+	}
+	return dist;
+}
 
 var Parser = createParser(Options);
 
-Parser("@import url('~test/css') screen and print;\n.class { a: b c d; }", function (next, it) {
-	console.log(it, this);
+var bsFonts = [
+	"@font-face {",
+	"    font-family: 'Glyphicons Halflings';",
+	"    src: url('../fonts/glyphicons-halflings-regular.eot');",
+	"    src: url('../fonts/glyphicons-halflings-regular.eot?#iefix') format('embedded-opentype'), url('../fonts/glyphicons-halflings-regular.woff2') format('woff2'), url('../fonts/glyphicons-halflings-regular.woff') format('woff'), url('../fonts/glyphicons-halflings-regular.ttf') format('truetype'), url('../fonts/glyphicons-halflings-regular.svg#glyphicons_halflingsregular') format('svg');",
+	"}"
+].join('\n');
+
+var cssText = [
+	".class { background: green url( \"img.png\" ) xyz }",
+	//".class { background: green url(data:image/png;base64,AAA) url(http://example.com/image.jpg) url(//example.com/image.png) xyz }",
+	//"@import url('path/test.css') screen and print;\n.class { a: b c d; }",
+	//bsFonts,
+	""
+].join('\n');
+
+Parser(cssText, function (next, it, control) {
+	if (!Url.isMemoryUrl(it.url)) {
+		var emitResult = control.emitFilters('fileType', this, function(filter){
+			return filter(it);
+		});
+		console.log(it);
+	}
 	next();
 }, function (err, css) {
 	col.done(css);
 }, Context);
 
+
+
 /*
+
+
 
 	资源统一定位到一个目录
 		
